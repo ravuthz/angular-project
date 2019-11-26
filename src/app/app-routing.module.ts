6e049b98d401934ac6df0cf68360966f289df6c3
Vannaravuth YO <ravuthz@gmail.com>
2019-11-27 00:27:18 +0700

    Generate core & feature modules with routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'backend', loadChildren: () => import('./backend/backend.module').then(m => m.BackendModule) }, { path: 'frontend', loadChildren: () => import('./frontend/frontend.module').then(m => m.FrontendModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
